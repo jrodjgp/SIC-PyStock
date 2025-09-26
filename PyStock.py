e47b526d93e2abbdc66a43e1d366550db2f8ad1a
Jose Rodriguez <jrodjpg@gmail.com>
2025-09-26 11:10:59 -0500

    primer commit

--- PyStock.py
+++ PyStock.py
@@ -1,40 +1,33 @@
 import sqlite3
+#pense en utilizar sqllite ya que asi es directo en python
+# en vez de conectarlo con una base de datos afuera
 
 db_file = 'PyStock.db'
 
-# 1. Conectamos a la base de datos
 conexion = sqlite3.connect(db_file)
 cursor = conexion.cursor()
 
-# 2. Creamos la tabla (SOLO SI NO EXISTE YA)
+#primera tabla de producto, 
 cursor.execute("""
-CREATE TABLE IF NOT EXISTS usuarios (
-    nombre TEXT,
-    edad INTEGER
+CREATE TABLE IF NOT EXISTS productos (
+    id_producto    INTEGER PRIMARY KEY AUTOINCREMENT,
+    nombre         TEXT NOT NULL,
+    sku            TEXT UNIQUE,
+    precio         REAL,
+    cantidad_stock INTEGER
 )
 """)
 
-# 3. Insertamos algunos datos para asegurarnos de que no esté vacía
-# (Puedes comentar estas líneas después de la primera ejecución)
-cursor.execute("INSERT INTO usuarios (nombre, edad) VALUES (?, ?)", ("Carlos", 40))
-cursor.execute("INSERT INTO usuarios (nombre, edad) VALUES (?, ?)", ("Beatriz", 25))
+cursor.execute("SELECT * FROM productos")
+todosProductos = cursor.fetchall()
 
-# Guardamos las inserciones
-conexion.commit()
+if todosProductos:
+    for id_p, nombre, sku, precio, stock in todosProductos:
+        print(f"ID: {id_p}")
+        print(f"  Nombre: {nombre}")
+        print(f"  SKU: {sku}")
+        print(f"  Precio: ${precio}")
+        print(f"  Stock: {stock} unidades")
 
-# 4. Seleccionamos y mostramos todos los datos
-print("Recuperando todos los datos de la tabla 'usuarios'...")
-cursor.execute("SELECT * FROM usuarios")
-todos_los_usuarios = cursor.fetchall()
-
-print("--- Usuarios Encontrados ---")
-if todos_los_usuarios:
-    for usuario in todos_los_usuarios:
-        print(f"Nombre: {usuario[0]}, Edad: {usuario[1]}")
-else:
-    print("No se encontraron usuarios.")
-print("--------------------------")
-
-
-# 5. Cerramos la conexión
+#recordar siempre cerrar la conexion
 conexion.close()
